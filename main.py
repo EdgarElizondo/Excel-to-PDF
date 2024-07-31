710909316a413efdb61e4e5339b72e0597befc2f
EdgarElizondo <edgar_7_23@hotmail.com>
2024-07-31 12:08:21 -0600

    Create a PDF file for each excel

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import glob
 import pandas as pd
+from fpdf import FPDF
+from pathlib import Path
 
-
+# Get EXCEL filepaths
 filepaths = glob.glob("src/*xlsx")
-
 for filepath in filepaths:
+    # Load Excel into dataframes
     data = pd.read_excel(filepath, sheet_name="Sheet 1")
-    print(data)
+    # Creates PDF
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf.add_page()
+    filename = Path(filepath).stem
+    document_num = filename.split("-")[0]
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Invoice num. {document_num}", align="L", ln=1)
+
+    pdf.output(f"PDFs/{filename}.pdf")
