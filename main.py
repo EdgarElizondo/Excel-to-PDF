ec82f5b4b8e79904b259e548035179bd7dcdf871
EdgarElizondo <edgar_7_23@hotmail.com>
2024-07-31 22:00:06 -0600

    Add table to PDF

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ from pathlib import Path
 
 # Get EXCEL filepaths
 filepaths = glob.glob("src/*xlsx")
+
 for filepath in filepaths:
-    # Load Excel into dataframes
-    data = pd.read_excel(filepath, sheet_name="Sheet 1")
     # Creates PDF
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
@@ -20,6 +19,26 @@ for filepath in filepaths:
     # Date
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, txt=f"Date: {date}", align="L", ln=1)
+    
+    # Load Excel into dataframes
+    data = pd.read_excel(filepath, sheet_name="Sheet 1")
+    # Table
+    columns_width = [30,50,50,30,30]
+    # Header
+    for index, column in enumerate(data.columns):
+        pdf.set_font(family="Times", size=12, style="B")
+        pdf.set_text_color(0, 0, 0)    
+        col_name = column.replace("_"," ").title()
+        pdf.cell(w=columns_width[index], h=10, txt=f"{col_name}", border=1)
+    pdf.cell(w=50, h=10,txt="",ln=1)
+
+    # Values
+    for index, row in data.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(50, 50, 50)
+        for index, value in enumerate(row.values):
+            pdf.cell(w=columns_width[index], h=10, txt=f"{value}", border=1)
+        pdf.cell(w=50, h=10,txt="",ln=1)
 
 
     pdf.output(f"PDFs/{filename}.pdf")
