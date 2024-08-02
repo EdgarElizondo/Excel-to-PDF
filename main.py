2afed731211533a7c518cfa395c6ee7daa64de4e
EdgarElizondo <edgar_7_23@hotmail.com>
2024-08-02 09:02:49 -0600

    Add total sum, company name and logo

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fpdf import FPDF
 from pathlib import Path
 
 # Get EXCEL filepaths
-filepaths = glob.glob("src/*xlsx")
+filepaths = glob.glob("src/Data/*xlsx")
 
 for filepath in filepaths:
     # Creates PDF
@@ -23,22 +23,40 @@ for filepath in filepaths:
     # Load Excel into dataframes
     data = pd.read_excel(filepath, sheet_name="Sheet 1")
     # Table
-    columns_width = [30,50,50,30,30]
+    columns_width = [30,70,30,30,30]
     # Header
+    pdf.set_font(family="Times", size=12, style="B")
+    pdf.set_text_color(0, 0, 0)    
     for index, column in enumerate(data.columns):
-        pdf.set_font(family="Times", size=12, style="B")
-        pdf.set_text_color(0, 0, 0)    
         col_name = column.replace("_"," ").title()
         pdf.cell(w=columns_width[index], h=10, txt=f"{col_name}", border=1)
     pdf.cell(w=50, h=10,txt="",ln=1)
 
     # Values
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(50, 50, 50)
     for index, row in data.iterrows():
-        pdf.set_font(family="Times", size=10)
-        pdf.set_text_color(50, 50, 50)
         for index, value in enumerate(row.values):
             pdf.cell(w=columns_width[index], h=10, txt=f"{value}", border=1)
         pdf.cell(w=50, h=10,txt="",ln=1)
+    
+    # Total Values
+    total_sum = data[column].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(50, 50, 50)    
+    for index, column in enumerate(data.columns):
+        if column == "total_price":
+            pdf.cell(w=columns_width[index], h=10, txt=f"{total_sum}", border=1)
+        else:
+            pdf.cell(w=columns_width[index], h=10, txt="", border=1)
+    pdf.cell(w=50, h=10,txt="",ln=1)
 
-
-    pdf.output(f"PDFs/{filename}.pdf")
+    # Total sum sentences
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=8, txt=f"Total price is {total_sum}", ln=1)
+    # Campany Name and logo
+    pdf.set_font(family="Times", size=14, style="B")
+    pdf.cell(w=35, h=8, txt=f"2-Ring Python")
+    pdf.image("src/Images/pythonlogo.png", w=10)
+    
+    pdf.output(f"PDFs/{filename}.pdf") 
